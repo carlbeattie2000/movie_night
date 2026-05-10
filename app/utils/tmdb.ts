b44408945948d76ae46f1db82a20450d49644097
Carl Beattie <contact.carlbeattie@gmail.com>
2026-05-10 14:30:31 +0100

    feat: tmdb fetch helpers

---
+++ app/utils/tmdb.ts
@@ -0,0 +1,20 @@
+import env from '#start/env'
+
+const BASE_URL = 'https://api.themoviedb.org/3'
+
+async function tmdbFetch(path: string) {
+  const res = await fetch(`${BASE_URL}${path}`, {
+    headers: { Authorization: `Bearer ${env.get('TMDB_KEY')}` },
+  })
+
+  if (!res.ok) throw new Error(`TMDB error: ${res.status}`)
+
+  return res.json()
+}
+
+export const tmdb = {
+  search: (query: string) =>
+    tmdbFetch(`/search/movie?query=${encodeURIComponent(query)}&language=en-US`),
+  genres: () => tmdbFetch('/genre/movie/list?language=en-US'),
+  movie: (tmdbId: number) => tmdbFetch(`/movie/${tmdbId}?language=en-US`),
+}
