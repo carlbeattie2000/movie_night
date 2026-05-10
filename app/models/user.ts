0151ceaeba63ee4baba89f3c69f1feb9b011b611
Carl Beattie <contact.carlbeattie@gmail.com>
2026-05-10 12:56:45 +0100

    feat: add relations on model

--- app/models/user.ts
+++ app/models/user.ts
@@ -2,23 +2,15 @@ import { UserSchema } from '#database/schema'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { manyToMany } from '@adonisjs/lucid/orm'
+import Movie from './movie.ts'
+import type { ManyToMany } from '@adonisjs/lucid/types/relations'
 
-/**
- * User model represents a user in the application.
- * It extends UserSchema and includes authentication capabilities
- * through the withAuthFinder mixin.
- */
 export default class User extends compose(UserSchema, withAuthFinder(hash)) {
-  /**
-   * Get the user's initials from their full name or email.
-   * Returns the first letter of first and last name if available,
-   * otherwise returns the first two characters of the email username.
-   */
-  get initials() {
-    const [first, last] = this.fullName ? this.fullName.split(' ') : this.email.split('@')
-    if (first && last) {
-      return `${first.charAt(0)}${last.charAt(0)}`.toUpperCase()
-    }
-    return `${first.slice(0, 2)}`.toUpperCase()
-  }
+  @manyToMany(() => Movie, {
+    pivotTable: 'watchlist_items',
+    pivotColumns: ['watched', 'last_watched'],
+    pivotTimestamps: true,
+  })
+  declare watchlist_items: ManyToMany<typeof Movie>
 }
