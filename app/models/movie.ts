c9dba71e5f39cc3a372d7cdb770de69507e5089f
Carl Beattie <contact.carlbeattie@gmail.com>
2026-05-10 12:56:45 +0100

    feat: add relations on model

--- app/models/movie.ts
+++ app/models/movie.ts
@@ -1,4 +1,13 @@
 import { MovieSchema } from '#database/schema'
+import { manyToMany } from '@adonisjs/lucid/orm'
+import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+import User from './user.ts'
 
 export default class Movie extends MovieSchema {
+  @manyToMany(() => User, {
+    pivotTable: 'watchlist_items',
+    pivotColumns: ['watched', 'last_watched', 'created_at'],
+    pivotTimestamps: true,
+  })
+  declare watchlist: ManyToMany<typeof User>
 }
