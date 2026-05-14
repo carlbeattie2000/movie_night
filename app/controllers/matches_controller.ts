b01a436f518dc0d02c9410f2cd2c08541c5e69cc
Carl Beattie <contact.carlbeattie@gmail.com>
2026-05-14 13:26:36 +0100

    feat: game rooms changed to support our socket approach

--- app/controllers/matches_controller.ts
+++ app/controllers/matches_controller.ts
@@ -1,66 +1,10 @@
 import MoviePickedResult from '#models/movie_picked_result'
 import WatchlistItem from '#models/watchlist_item'
-import selectionService from '#services/selection_service'
 import type { HttpContext } from '@adonisjs/core/http'
-import transmit from '@adonisjs/transmit/services/main'
-import { DateTime } from 'luxon'
-import { tmdb } from '../utils/tmdb.ts'
 
 export default class MatchesController {
   async lobby({ inertia }: HttpContext) {
     return inertia.render('lobby/index', {})
-  }
-
-  async ready({ auth, response }: HttpContext) {
-    const user = auth.getUserOrFail()
-    selectionService.setReady(user.id)
-
-    if (selectionService.bothReady()) {
-      const winnerId = selectionService.pick()
-
-      await WatchlistItem.query()
-        .whereIn('userId', [1, 2])
-        .where('movieId', winnerId)
-        .update({ watched: true, lastWatched: DateTime.now().toSQL() })
-
-      await WatchlistItem.query()
-        .whereIn('userId', [1, 2])
-        .where('movieId', winnerId)
-        .whereNull('firstWatchedAt')
-        .update({ firstWatchedAt: DateTime.now().toSQL() })
-
-      await MoviePickedResult.create({
-        movieId: winnerId,
-      })
-
-      transmit.broadcast('result', { winner: winnerId })
-      selectionService.clear()
-    }
-
-    return response.noContent()
-  }
-
-  async result({ view, response, session }: HttpContext) {
-    const lastPicked = await MoviePickedResult.query()
-      .orderBy('createdAt', 'desc')
-      .preload('movie')
-      .first()
-
-    if (!lastPicked) {
-      return response.badRequest('You must pick a movie before viewing the result.')
-    }
-
-    const providersResult = await tmdb.providers(lastPicked.movie.tmdbId)
-
-    if (providersResult.status === 'invalid_json') {
-      session.flash('error', providersResult.message)
-      return view.render('pages/movies/picked', { movie: lastPicked.movie })
-    }
-
-    return view.render('pages/movies/picked', {
-      movie: lastPicked.movie,
-      providers: providersResult.result.results?.GB?.flatrate,
-    })
   }
 
   async cancel({ response }: HttpContext) {
--- app/controllers/movies/selects_controller.ts
+++ app/controllers/movies/selects_controller.ts
@@ -1,16 +1,12 @@
-import selectionService from '#services/selection_service'
+import gameRooms from '#services/game_rooms_service'
 import type { HttpContext } from '@adonisjs/core/http'
-import transmit from '@adonisjs/transmit/services/main'
 
 export default class SelectsController {
   async store({ auth, request, response }: HttpContext) {
     const user = auth.getUserOrFail()
     const movieId = request.input('movie_id')
 
-    selectionService.set(user.id, movieId)
-    transmit.broadcast('selections', {
-      selections: selectionService.get(),
-    })
+    gameRooms.set('main', user.id, movieId)
 
     return response.redirect().toRoute('matches.lobby')
   }
@@ -22,10 +18,7 @@ export default class SelectsController {
 
     const movie = watchListMovies[Math.floor(Math.random() * watchListMovies.length)]
 
-    selectionService.set(user.id, movie.id)
-    transmit.broadcast('selections', {
-      selections: selectionService.get(),
-    })
+    gameRooms.set('main', user.id, movie.id)
 
     return response.redirect().toRoute('matches.lobby')
   }
