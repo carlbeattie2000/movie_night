cdaf8fb462fb1f0d4d2795c3cb81b63751f8f60c
Carl Beattie <contact.carlbeattie@gmail.com>
2026-05-18 23:41:38 +0100

    fix: drop uncommon letters

--- app/engines/word_game.ts
+++ app/engines/word_game.ts
@@ -5,7 +5,7 @@ import app from '@adonisjs/core/services/app'
 
 const WORDS = readFileSync(join(app.makePath('resources/data/words.txt')), 'utf-8').split('\n')
 const VOWELS = 'aeiou'.split('')
-const CHARS = 'abcdefghijklmnopqrstuvwxyz'.split('').filter((char) => !VOWELS.includes(char))
+const CHARS = 'abcdefghiklmnoprstuvy'.split('').filter((char) => !VOWELS.includes(char))
 
 const MIN_WORDS_BEFORE_START = 60
 
