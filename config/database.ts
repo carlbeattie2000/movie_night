beeb756d01b4c0af8534aa5d43bbc9baa1381425
Carl Beattie <contact.carlbeattie@gmail.com>
2026-05-18 21:27:21 +0100

    fix: database use node_env to decide if pretty print

--- config/database.ts
+++ config/database.ts
@@ -11,7 +11,7 @@ const dbConfig = defineConfig({
   /**
    * Pretty-print SQL debug output in development logs.
    */
-  prettyPrintDebugQueries: false,
+  prettyPrintDebugQueries: env.get('NODE_ENV') === 'development',
 
   connections: {
     /**
